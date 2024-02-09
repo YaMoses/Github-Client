c14dbfc544777c560ab513699de0bc333a203c7c
Elyamus <ysdtuu@gmail.com>
2024-02-09 12:22:57 +0100

    DiffableCollectionView on FollowersViewController setup

---
+++ Github Client/FollowersCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  FollowersCollectionViewCell.swift
+//  Github Client
+//
+//  Created by Yamoses on 09/02/2024.
+//
+
+import UIKit
+
+class FollowersCollectionViewCell: UICollectionViewCell {
+    
+}
